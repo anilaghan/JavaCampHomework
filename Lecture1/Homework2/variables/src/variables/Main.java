e04c2fabb55a80022c99c35b7d9ed4640e78aa95
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-01 17:08:33 +0300

    Lecture1/Homework2

---
+++ Lecture1/Homework2/variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 10;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım :   " + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım :   " + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım :   " + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım :   " + ogrenciSayisi);
+
+	}
+
+}
