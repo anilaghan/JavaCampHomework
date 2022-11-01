87ecf1095469733c44a417bfa71ee0be3981b907
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-01 17:17:05 +0300

    /Lecture1/Homework2

---
+++ Lecture1/Homework2/conditionals/src/conditionals/Main.java
@@ -0,0 +1,17 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 24;
+		
+		if(sayi<20) {
+			System.out.println("Sayı 20 den küçüktür");
+		}else if(sayi==20){
+			System.out.println("Sayı 20ye eşittir");
+		}else {
+			System.out.println("Sayı 20 den küçük değildir.");
+		}
+	}
+
+}
