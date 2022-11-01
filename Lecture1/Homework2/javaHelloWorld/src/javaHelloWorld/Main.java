b8d3032c0fd991a9817de490734ace71548e0913
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-01 17:16:31 +0300

    /Lecture1/Homework2

---
+++ Lecture1/Homework2/javaHelloWorld/src/javaHelloWorld/Main.java
@@ -0,0 +1,11 @@
+package javaHelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java 2");
+
+	}
+
+}
