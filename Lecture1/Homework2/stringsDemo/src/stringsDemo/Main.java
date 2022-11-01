cab61f62505f87fba53b51a06f071d3f8ade09a1
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-01 17:12:09 +0300

    /Lecture1/Homework2

---
+++ Lecture1/Homework2/stringsDemo/src/stringsDemo/Main.java
@@ -0,0 +1,35 @@
+package stringsDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String mesaj = "   Bugün hava çok güzel.   ";
+		System.out.println(mesaj);
+		/*
+		System.out.println("Eleman Sayısı: " + mesaj.length());
+		System.out.println("5. elaman: " + mesaj.charAt(4));
+		System.out.println(mesaj.concat(" Yaşasın!"));
+		System.out.println(mesaj.startsWith("b"));
+		System.out.println(mesaj.endsWith("."));
+		char[] karakterler = new char[5];
+		mesaj.getChars(0, 5, karakterler, 0);
+		System.out.println(karakterler);
+		System.out.println(mesaj.indexOf('a'));
+		System.out.println(mesaj.lastIndexOf('a'));
+		*/
+		
+		String yeniMesaj = mesaj.replace(' ','-');
+		System.out.println(yeniMesaj);
+		System.out.println(mesaj.substring(2,4));
+		
+		
+		for(String kelime : mesaj.split(" ")) {
+			System.out.println(kelime);
+		}
+		
+		System.out.println(mesaj.toLowerCase());
+		System.out.println(mesaj.toUpperCase());
+		System.out.println(mesaj.trim());
+	}
+
+}
