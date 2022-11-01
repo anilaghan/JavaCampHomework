c2995293d8d39e9c918bfbee5e151280398d5995
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-01 17:15:32 +0300

    /Lecture1/Homework2

---
+++ Lecture1/Homework2/mukemmelSayi/src/mukemmelSayi/Main.java
@@ -0,0 +1,21 @@
+package mukemmelSayi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 27;
+		int total = 0;
+
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				total = total + i;
+			}
+		}
+		if (total == number) {
+			System.out.println("Mükemmel Sayıdır");
+		} else {
+			System.out.println("Mükemmel Sayı Değildir.");
+		}
+	}
+
+}
