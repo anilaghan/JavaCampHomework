60e629700154a7a3343b33c5159e7e8ebce7ab10
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-05 00:59:12 +0300

    /Lecture3/Homework2

---
+++ Lecture3/Homework2/polymorphismDemo/src/polymorphismDemo/Main.java
@@ -0,0 +1,16 @@
+package polymorphismDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		BaseLogger[] loggers = {new FileLogger(),new EmailLogger(),new DatabaseLogger(),new ConsoleLogger()};
+//		for(BaseLogger logger:loggers) {
+//			logger.log("Log Mesajı ");
+//		}
+		
+		CustomerManager customerManager= new CustomerManager(new FileLogger());
+		customerManager.add();
+		
+	}
+
+}
