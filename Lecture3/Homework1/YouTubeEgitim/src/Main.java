5572ab1be36191404b4f232175e99f1ad6fb1c74
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-04 15:45:39 +0300

    /Lecture3/Homework1

---
+++ Lecture3/Homework1/YouTubeEgitim/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] sayilar1 = { 1, 2, 3 };
+		int[] sayilar2 = { 10, 20, 30 };
+		sayilar1 = sayilar2;
+		
+		sayilar2[0] = 1000;
+		
+
+		System.out.println(sayilar1[0]);
+
+	}
+
+}
