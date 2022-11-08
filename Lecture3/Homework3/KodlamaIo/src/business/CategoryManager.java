1592e7ad18d2e5cd630dfcaa70fe3041b45ae1d2
Anıl Ağhan <anilaghan@gmail.com>
2022-11-08 22:45:45 +0300

    /Lecture3/Homework3

--- Lecture3/Homework3/KodlamaIo/src/business/CategoryManager.java
+++ Lecture3/Homework3/KodlamaIo/src/business/CategoryManager.java
@@ -11,35 +11,35 @@ public class CategoryManager {
 	private List<Category> categories;
 	private Logger[] loggers;
 
-	public CategoryManager(CategoryDao categoryDao, List<Category> categories,Logger[] loggers) {
+	public CategoryManager(CategoryDao categoryDao, List<Category> categories, Logger[] loggers) {
 		this.categoryDao = categoryDao;
 		this.categories = categories;
-		this.loggers=loggers;
+		this.loggers = loggers;
 	}
 
 	public void add(Category category) throws Exception {
 		for (Category category1 : categories) {
 			if (category1.getName() == category1.getName()) {
 				throw new Exception("Bu isimle bir kategori bulunmaktadır. Lütfen farklı bir isim giriniz.");
-			} else {
-				categoryDao.add(category);
 			}
+
 		}
-		for(Logger logger:loggers) {
+		categoryDao.add(category);
+		for (Logger logger : loggers) {
 			logger.log();
 		}
 	}
 
 	public void delete(Category category) {
 		categoryDao.update(category);
-		for(Logger logger:loggers) {
+		for (Logger logger : loggers) {
 			logger.log();
 		}
 	}
 
 	public void update(Category category) {
 		categoryDao.delete(category);
-		for(Logger logger:loggers) {
+		for (Logger logger : loggers) {
 			logger.log();
 		}
 	}
