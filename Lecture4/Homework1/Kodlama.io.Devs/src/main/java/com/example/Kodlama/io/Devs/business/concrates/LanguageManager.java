05b489b64eba890a5cbdc2099e8e3f65ae295c90
lynafox <anilaghan@gmail.com>
2022-11-10 22:43:59 +0300

    /Deneme

--- Lecture4/Homework1/Kodlama.io.Devs/src/main/java/com/example/Kodlama/io/Devs/business/concrates/LanguageManager.java
+++ Lecture4/Homework1/Kodlama.io.Devs/src/main/java/com/example/Kodlama/io/Devs/business/concrates/LanguageManager.java
@@ -45,7 +45,7 @@ public class LanguageManager implements LanguageService {
 			if (language2.getId() == id || name.isEmpty()) {
 				throw new Exception("Hatalı Giriş Yaptınız, Bilgilerinizi Kontrol Ediniz.");
 			}
-			languageRepository.updaate(language, id, name);
+			languageRepository.update(language, id, name);
 		}
 	}
 
